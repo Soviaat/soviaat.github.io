43767766ecde4f60dfd6132042c30f201d17e387
Soviaat <94050783+Soviaat@users.noreply.github.com>
2024-11-02 13:51:27 +0100

    Added dropdown

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ window.onload = function() {
         subFolders.forEach(f => {
             const linkContainer = document.createElement("div");
             linkContainer.className = "link-container";
+            linkContainer.id = `${f}`;
+
+            if(linkContainer.id == "mods") {
+                const plusButton = document.createElement("div")
+                plusButton.classList.add("plus");
+                plusButton.id = "plus-btn";
+                plusButton.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M480-344 240-584l47.33-47.33L480-438.67l192.67-192.66L720-584 480-344Z"/></svg>';
+                linkContainer.appendChild(plusButton)
+            }
 
             const link = document.createElement("a");
             link.href = `./${f}`
@@ -22,6 +31,43 @@ window.onload = function() {
     }
 
     createLinks();
+
+    const repoOwner = 'soviaat';
+    const repositories = [
+        { name: 'Statify'},
+        { name: 'VexSoundPause' }
+    ];
+
+    function createList() {
+        const list = document.createElement("ul");
+        list.className = "hidden";
+        list.id = "repo-dropdown";
+        repositories.forEach(r => {        
+            list.innerHTML += 
+            `<a href="/mods?id=${r.name}">
+                <li>${r.name}</li>
+            </a>`;
+        });
+
+        document.getElementById("main").appendChild(list);
+    }
+
+    createList();
+
+    
+    const plus = document.getElementById("plus-btn");
+    const list = document.getElementById("repo-dropdown");
+    plus.addEventListener("click", () => {
+        if(list.classList.contains("hidden")) {
+            plus.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M480-545.33 287.33-352.67 240-400l240-240 240 240-47.33 47.33L480-545.33Z"/></svg>';
+            list.classList.remove("hidden");
+            list.classList.toggle("shown");
+        } else {
+            plus.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M480-344 240-584l47.33-47.33L480-438.67l192.67-192.66L720-584 480-344Z"/></svg>';
+            list.classList.remove("shown");
+            list.classList.toggle("hidden");
+        }
+    });
 };
 
 
