ef4e289e75274a7df26986d7d8b88b77beb68a6b
Soviaat <94050783+Soviaat@users.noreply.github.com>
2025-01-18 12:21:02 +0100

    Close to being finished

--- js/statify.js
+++ js/statify.js
@@ -1,11 +1,21 @@
-
-
 window.addEventListener("load", function() {
-
-    const slider = document.querySelector(".slider");
+    const popup = document.getElementById("cookie-popup");
+    const closeBtn = document.getElementById("close-cookie-popup");
+    const cookieKey = "cookie-popup-dismissed";
     const nav = document.querySelector(".navigation");
     const menuIcon = document.querySelector(".menu-icon");
     const slides = document.querySelectorAll(".slider img");
+    const slider = document.querySelector(".slider");
+    const install = document.querySelector(".install");
+    
+    if(!localStorage.getItem(cookieKey)) {
+        popup.style.display = "flex";
+    }
+
+    closeBtn.addEventListener("click", () => {
+        popup.style.display = "none";
+        localStorage.setItem(cookieKey, "true");
+    });
 
     this.window.addEventListener("resize", () => {
         if(window.innerWidth <= 600) {
@@ -67,7 +77,10 @@ window.addEventListener("load", function() {
     const codeElements = [
         ["fabric", "https://fabricmc.net/"],
         ["fabric-download", "https://www.curseforge.com/minecraft/mc-mods/fabric-api/files/5848117"],
-        ["fabric-loader-download", "https://github.com/FabricMC/fabric-loader/releases/tag/0.16.9"]
+        ["fabric-loader-download", "https://github.com/FabricMC/fabric-loader/releases/tag/0.16.9"],
+        ["luckey", "https://twitch.tv/luckeY"],
+        ["statify", "https://soviaat.dev/mods?id=statify"],
+        ["gitrepo", "https://github.com/Soviaat/Statify"]
     ]
 
     codeElements.forEach(([id, link]) => {
@@ -75,8 +88,12 @@ window.addEventListener("load", function() {
         if (target) {
             addTooltip(target, "tooltip", link);
         } else {
-            log(`Element with the if of \`${id}\` could not be found.`);
+            log(`Element with the id of \`${id}\` could not be found.`);
         }
+    });
+
+    install.addEventListener("click", () => {
+        window.open("https://soviaat.dev/mods?id=Statify");
     });
 
 });
@@ -107,6 +124,7 @@ function addTooltip(target, CSSelement, link) {
     target.addEventListener("click", () => {
         window.open(link, "_blank");
     });
+
 }
 
 function log(string) {
