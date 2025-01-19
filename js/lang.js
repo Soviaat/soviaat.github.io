66d8aeb6cc60713bccfae2a9c0bbdbe48e9c3af7
Soviaat <94050783+Soviaat@users.noreply.github.com>
2025-01-19 17:34:02 +0100

    Added language selection

---
+++ js/lang.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const langSelector = document.querySelector(".dropdown-selected");
+    const toTranslate = document.querySelectorAll("[data-translate-key]");
+    
+    function loadLang(lang) {
+        fetch(`/lang/${lang}.json`)
+            .then(resp => resp.json())
+            .then(translations => {
+                toTranslate.forEach(el => {
+                    const key = el.getAttribute('data-translate-key');
+                    if(translations[key]) {
+                        const translation = translations[key];
+                        if (translation.type === "html") {
+                            el.innerHTML = translation.content;
+                        } else {
+                            el.textContent = translation.content;
+                        }
+                    }
+                });
+
+                setupTooltips();
+            });
+    }
+    
+    document.querySelectorAll('.dropdown-option').forEach(option => {
+        option.addEventListener('click', function () {
+            const selectedValue = this.getAttribute('value');
+            langSelector.setAttribute('value', selectedValue);
+            langSelector.textContent = this.textContent;
+
+            // Load the selected language
+            loadLang(selectedValue);
+        });
+    });
+    
+    loadLang('en');
+
+    function setupTooltips() {
+        const codeElements = [
+                ["fabric", "https://fabricmc.net/"],
+                ["fabric-download", "https://www.curseforge.com/minecraft/mc-mods/fabric-api/files/6036511"],
+                ["fabric-loader-download", "https://github.com/FabricMC/fabric-loader/releases/tag/0.16.9"],
+                ["luckey", "https://twitch.tv/luckeY"],
+                ["statify", "https://soviaat.dev/mods?id=statify"],
+                ["gitrepo", "https://github.com/Soviaat/Statify"],
+                ["readme", "https://github.com/Soviaat/Statify/blob/master/README.md"],
+                ["git-issue", "https://github.com/Soviaat/Statify/issues/new"]
+            ]
+            
+            codeElements.forEach(([id, link]) => {
+                const target = this.document.getElementById(id);
+                if (target) {
+                    addTooltip(target, "tooltip", link);
+                } else {
+                    log(`Element with the id of \`${id}\` could not be found.`);
+                }
+        });
+    }
+
+    function addTooltip(target, CSSelement, link) {
+        const tooltip = this.document.createElement("div");
+        tooltip.className = CSSelement;
+        tooltip.innerText = link;
+        this.document.body.appendChild(tooltip);
+    
+        const tipStyle = tooltip.style;
+    
+        target.addEventListener("mouseover", (e) => {
+            tipStyle.display = "block";
+            tipStyle.left = `${e.pageX + 13}px`;
+            tipStyle.top = `${e.pageY + 13}px`;
+        });
+    
+        target.addEventListener("mousemove", (e) => {
+                tipStyle.left = `${e.pageX + 13}px`;
+                tipStyle.top = `${e.pageY + 13}px`;
+        });
+    
+        target.addEventListener("mouseout", (e) => {
+                tipStyle.display = "none";
+            });
+    
+        target.addEventListener("click", () => {
+            window.open(link, "_blank");
+        });
+    }
+});
+
--- js/statify.js
+++ js/statify.js
@@ -1,30 +1,60 @@
 document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('.dropdown-selected').addEventListener('click', function () {
+        const dropdown = this.parentElement;
+        dropdown.classList.toggle('show');
+    });
+    
+    document.querySelectorAll('.dropdown-option').forEach(option => {
+        option.addEventListener('click', function () {
+            const selectedText = this.textContent;
+            const selectedValue = this.getAttribute("value");
+            const dropdown = this.closest('.dropdown');
+            dropdown.querySelector('.dropdown-selected').innerText = selectedText;
+            dropdown.querySelector('.dropdown-selected').setAttribute("value", selectedValue);
+            dropdown.classList.remove('show');
+        });
+    });
+    
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('.dropdown')) {
+            document.querySelectorAll('.dropdown').forEach(dropdown => dropdown.classList.remove('show'));
+        }
+    });
+
+
     const loadingBar = document.querySelector(".loading-bar");
     const percent = document.getElementById("percent");
-    loadingBar.style.width = "100%";
 
+    loadingBar.style.width = "100%";
+    
     function getCurrentWidthPercentage(element) {
         const elementWidth = element.getBoundingClientRect().width;
         const parentWidth = element.parentElement.getBoundingClientRect().width;
-
+        
         return (elementWidth / parentWidth) * 100;
     }
-
+    
     function trackWidthDuringTransition(element, target) {
+        let animationFrameId;
+        
         const updateWidth = () => {
             const currentWidth = getCurrentWidthPercentage(element);
-            requestAnimationFrame(updateWidth);
             target.innerText = Math.floor(currentWidth) + "%";
-            if(Math.floor(currentWidth) === 100) {
-                cancelAnimationFrame(updateWidth);
+            
+            if(Math.floor(currentWidth) === 100 ) {
+                cancelAnimationFrame(animationFrameId);
                 log("ended");
+            } else {
+                animationFrameId = requestAnimationFrame(updateWidth);
             }
-        }
-
-        requestAnimationFrame(updateWidth);
+        };
+        
+        animationFrameId = requestAnimationFrame(updateWidth);
     }
-
+    
+    
     trackWidthDuringTransition(loadingBar, percent);
+    
     setTimeout(() => {
         const loadingContainer = document.querySelector(".loading-container");
         loadingContainer.style.opacity = '0';
@@ -33,7 +63,9 @@ document.addEventListener("DOMContentLoaded", () => {
             loadingContainer.style.display = "none";
         }, 1000);
     }, 4350);
-    
+        
+
+        
     const popup = document.getElementById("cookie-popup");
     const closeBtn = document.getElementById("close-cookie-popup");
     const cookieKey = "cookie-popup-dismissed";
@@ -42,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const slides = document.querySelectorAll(".slider img");
     const slider = document.querySelector(".slider");
     const install = document.querySelector(".install");
-    
+
     const help = document.querySelector(".help-link");
     const helpContainer = document.querySelector(".help-container");
     const downloadLink = document.querySelector(".download-link");
@@ -87,7 +119,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if(window.innerWidth > 600) {
             nav.classList.remove("open");
         }
-    
     });
     
     if(window.innerWidth <= 600) {
@@ -131,68 +162,11 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     
     setInterval(autoScroll, 3000);
-    
-    const codeElements = [
-        ["fabric", "https://fabricmc.net/"],
-        ["fabric-download", "https://www.curseforge.com/minecraft/mc-mods/fabric-api/files/6036511"],
-        ["fabric-loader-download", "https://github.com/FabricMC/fabric-loader/releases/tag/0.16.9"],
-        ["luckey", "https://twitch.tv/luckeY"],
-        ["statify", "https://soviaat.dev/mods?id=statify"],
-        ["gitrepo", "https://github.com/Soviaat/Statify"],
-        ["readme", "https://github.com/Soviaat/Statify/blob/master/README.md"],
-        ["git-issue", "https://github.com/Soviaat/Statify/issues/new"]
-    ]
-    
-    codeElements.forEach(([id, link]) => {
-        const target = this.document.getElementById(id);
-        if (target) {
-            addTooltip(target, "tooltip", link);
-        } else {
-            log(`Element with the id of \`${id}\` could not be found.`);
-        }
-    });
-    
+
     install.addEventListener("click", () => {
         window.open("https://soviaat.dev/mods?id=Statify");
     });
 });
-
-function addTooltip(target, CSSelement, link) {
-    const tooltip = this.document.createElement("div");
-    tooltip.className = CSSelement;
-    tooltip.innerText = link;
-    this.document.body.appendChild(tooltip);
-
-    const tipStyle = tooltip.style;
-
-    target.addEventListener("mouseover", (e) => {
-        tipStyle.display = "block";
-        tipStyle.left = `${e.pageX + 13}px`;
-        tipStyle.top = `${e.pageY + 13}px`;
-    });
-
-    target.addEventListener("mousemove", (e) => {
-            tipStyle.left = `${e.pageX + 13}px`;
-            tipStyle.top = `${e.pageY + 13}px`;
-    });
-
-    target.addEventListener("mouseout", (e) => {
-            tipStyle.display = "none";
-        });
-
-    target.addEventListener("click", () => {
-        window.open(link, "_blank");
-    });
-
-}
-
-function log(string) {
-    console.log(
-        `%c[Statify]%c ${string}`, 
-        "color: #2690ec; font-weight: bold;", // Style for "[Statify]"
-        "color: inherit;"
-    )
-}
 
 function scrollToElement(list, targetList) {
     for (let i = 0; i < list.length; i++) {
@@ -204,4 +178,12 @@ function scrollToElement(list, targetList) {
             }
         });
     }
+}
+
+function log(string) {
+    console.log(
+        `%c[Statify]%c ${string}`, 
+        "color: #2690ec; font-weight: bold;", // Style for "[Statify]"
+        "color: inherit;"
+    )
 }
